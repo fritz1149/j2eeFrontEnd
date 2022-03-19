5ec6eb69474bcb4fd50f350599ace7c141b7517c
huahuaxiaomuzhu <56588699+huahuaxiaomuzhu@users.noreply.github.com>
2022-03-19 16:11:13 +0800

    稍微写一下

---
+++ src/components/PostPreview.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="post-preview">
+    <div class="preview-titlebox">
+        <div class="preview-title">{{postTitle}}</div>
+        <div class="preview-sender">{{postSender.userName}}</div>
+    </div>
+    <div class="preview-body">
+        <div class="content-holder">
+          <div class="preview-text">{{postContent}}</div>
+          <div class="preview-pic">
+            <img :src="'https://huahuaxiaomuzhu.oss-cn-beijing.aliyuncs.com'+postPicUrl+'?x-oss-process=image/resize,w_200'">
+          </div>
+        </div>
+      <div class="last-replied-time">
+        {{postLastRepliedTime}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostPreview",
+  props:{
+    postId:Number,
+    postTitle:String,
+    postContent:String,
+    postSendDate:Date,
+    postSender:Object,
+    postLastRepliedTime:Date,
+    postPicUrl:String
+  }
+}
+</script>
+
+<style scoped>
+.post-preview{
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  font-size: 12px;
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  color: #333;
+  line-height: 22px;
+  list-style: none;
+  box-sizing: content-box;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  min-height: 1px;
+  margin-left: 70px;
+}
+.preview-titlebox{
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  font-size: 12px;
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  color: #333;
+  line-height: 22px;
+  list-style: none;
+  box-sizing: content-box;
+  margin: 0;
+  padding: 0;
+  zoom: 1;
+}
+.preview-title{
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  color: #333;
+  line-height: 22px;
+  list-style: none;
+  box-sizing: content-box;
+  margin: 0;
+  padding: 0;
+  float: left!important;
+  width: 420px;
+  font-size: 14px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.preview-sender{
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  font-size: 12px;
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  line-height: 22px;
+  list-style: none;
+  box-sizing: content-box;
+  margin: 0;
+  padding: 0;
+  float: right!important;
+  width: 165px;
+  color: #999;
+}
+.preview-body{
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  font-size: 12px;
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  color: #333;
+  line-height: 22px;
+  list-style: none;
+  box-sizing: content-box;
+  margin: 0;
+  padding: 0;
+  zoom: 1;
+  padding-top: 3px;
+  position: relative;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,38 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <post-preview v-for="(p,i) in $data.posts"  :key="i" :post-sender="p.sender" :post-title="p.title" :post-content="p.content" :post-send-date="p.sendTime" :post-pic-url="p.imgUrl" :post-last-replied-time="p.lastRepliedTime"  ></post-preview>
   </div>
 </template>
 
 <script>
 
+import PostPreview from "@/components/PostPreview";
+import axios from "axios";
 export default {
   name: 'Home',
+  data:function (){
+    return {
+      user:{
+        userName:"huahuaxiaomuzhu"
+      },
+      posts:[]
+    }
+  },
   components: {
+    PostPreview
+  },
+  beforeCreate() {
+    axios.get("http://localhost:4396/section/index",{
+      params:{
+        id:4,
+        pageNum:1,
+        pageSize:10
+      }
+    }).then((res)=>{
+      console.log(res.data.data.list)
+      this.$data.posts=res.data.data.list
+    })
   }
 }
 </script>
