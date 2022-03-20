645237de2dd4d38795a76093d78be4bb1d8e9813
huahuaxiaomuzhu <56588699+huahuaxiaomuzhu@users.noreply.github.com>
2022-03-20 15:01:01 +0800

    调css中

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+Vue.prototype.OssUrl="https://huahuaxiaomuzhu.oss-cn-beijing.aliyuncs.com"
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <post-preview v-for="(p,i) in $data.posts" :key="i" :post-sender="p.sender" :post-title="p.title"
                   :post-content="p.content" :post-send-date="p.sendTime" :post-pic-url="p.imgUrl"
-                  :post-last-replied-time="p.lastRepliedTime"></post-preview>
+                  :post-last-replied-time="p.lastRepliedTime" :post-replied-count="p.repliedCount"></post-preview>
   </div>
 </template>
 
